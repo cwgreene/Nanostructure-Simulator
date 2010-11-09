f914f7df06439638d0e8266fb653ae175df15a07
Chris Greene <archgoon@gmail.com>
2010-11-08 21:38:28 -0800

    Modified options to accept new material arguments.

--- mcoptions.py
+++ mcoptions.py
@@ -1,4 +1,7 @@
 import sys,optparse
+import meshes
+import materials
+
 def get_options():
 	parser = optparse.OptionParser()
 	parser.add_option("-c",type="int",dest="num",default=200)
@@ -13,5 +16,13 @@ def get_options():
 	parser.add_option("--doping",type="float",dest="doping",default=10**-24)
 	parser.add_option("--tag",type="string",dest="tag",default="None")
 	parser.add_option("--dt",type="float",dest="dt",default=10**-13)
+	parser.add_option("--mesh",type="string",dest="mesh",
+				default="TriangleMesh")
+	parser.add_option("--materials",type="string",dest="materials",
+				default="silicon,silicon")
 	options,args = parser.parse_args(sys.argv)
+	options.mesh = eval("meshes."+options.mesh)
+	options.materials_list = []
+	for x in options.materials.split(","):
+		options.materials_list.append(eval("materials."+x))
 	return options
